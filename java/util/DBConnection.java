7dcb454c0d73d22a4e4d6896b01d75bcbb64c0fa
Dikshana K <dikshanakrishnasamy@gmail.com>
2026-01-09 23:30:22 +0530

    Change database password in DBConnection class
    Updated database password for connection.

--- java/util/DBConnection.java
+++ java/util/DBConnection.java
@@ -5,12 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // Hardcoded path for academic project simplicity. 
-    // In a real app, this should be in a config file or environment variable.
-    // MySQL Connection Configuration
     private static final String URL = "jdbc:mysql://localhost:3306/stack_change_db?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASS = "27deepak";
+    private static final String PASS = "diks@123";
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         try {
@@ -37,3 +34,4 @@ public class DBConnection {
         }
     }
 }
+
